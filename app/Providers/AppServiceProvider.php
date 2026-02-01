d6c8d5a934ff299a23089e10add7a16666bb3116
Mei <ixyzyooo27@gmail.com>
2026-02-01 21:13:43 +0700

    fix: redirect storage paths to tmp for vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,13 +7,43 @@ use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
-        //
+        
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Schema::defaultStringLength(191); 
+        
+        Schema::defaultStringLength(191);
+
+        
+        if (config('app.env') === 'production') {
+            
+            config(['view.compiled' => '/tmp/storage/framework/views']);
+
+            
+            config(['session.files' => '/tmp/storage/framework/sessions']);
+            
+            
+            config(['cache.stores.file.path' => '/tmp/storage/framework/cache']);
+
+            
+            if (!is_dir('/tmp/storage/framework/views')) {
+                mkdir('/tmp/storage/framework/views', 0755, true);
+            }
+            if (!is_dir('/tmp/storage/framework/sessions')) {
+                mkdir('/tmp/storage/framework/sessions', 0755, true);
+            }
+            if (!is_dir('/tmp/storage/framework/cache')) {
+                mkdir('/tmp/storage/framework/cache', 0755, true);
+            }
+        }
     }
 }
